5a656ded3fa632cf933dff7a1b7b2eeb98d50ad2
Andres afonso martin <andres.afonso21@gmail.com>
2021-02-11 11:19:59 +0100

    objeto notificaciones añadidas

---
+++ app/src/main/java/com/example/tableverse/objetos/Notificaciones.java
@@ -0,0 +1,43 @@
+package com.example.tableverse.objetos;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.tableverse.LoginActividad;
+import com.example.tableverse.R;
+
+public class Notificaciones {
+
+    public Notificaciones() {
+    }
+
+    public void crearNotificacion(Bitmap icon, String id, String title,
+                                                        String contenido, String textoLargo,
+                                                        PendingIntent pendingIntent, Context context){
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,
+                id)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(contenido)
+                .setLargeIcon(icon)
+                .setContentIntent(pendingIntent)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(textoLargo))
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_MAX);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(LoginActividad.createID(), builder.build());
+        
+    }
+
+
+
+}
